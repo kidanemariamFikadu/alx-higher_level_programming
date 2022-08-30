18495a090342a4843fc7e210203f0038a859ecfc
kidanemariam <kide.fikadu1@gmail.com>
2022-08-30 04:23:45 +0000

    calculator

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
+
     import sys
-    
     nargs = len(sys.argv) - 1
     if nargs != 3:
         print("Usage: ./100-my_calculator.py <a> <operator> <b>")
