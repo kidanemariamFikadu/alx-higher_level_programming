5a5e1aa23f7d303348a82ce3a1414f1c49ed871c
kidanemariam <kide.fikadu1@gmail.com>
2022-08-30 04:17:23 +0000

    args

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -5,12 +5,14 @@ if __name__ == "__main__":
 
     if i == 0:
         print("{} arguments.".format(i))
+    elif i == 1:
+        print("{} argument:".format(i))
     else:
         print("{} arguments:".format(i))
         
-        if i >= 1:
-            i = 0
-            for arg in sys.argv:
-                if i != 0:
-                    print("{}: {}".format(i, arg))
-                i += 1
+    if i >= 1:
+        i = 0
+        for arg in sys.argv:
+            if i != 0:
+                print("{}: {}".format(i, arg))
+            i += 1
