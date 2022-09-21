e17822562fe4312d11f1c80af1c2d6fc598d53d7
kidanemariam <kide.fikadu1@gmail.com>
2022-09-21 22:28:34 +0000

    square

--- 0x06-python-classes/5-square.py
+++ 0x06-python-classes/5-square.py
@@ -38,6 +38,4 @@ class Square:
             print()
         else:
             for i in range(self.__size):
-                for j in range(self.__size):
-                    print("#", end='')
-            print()
+                print("".join(["#" for j in range(self.__size)]))
