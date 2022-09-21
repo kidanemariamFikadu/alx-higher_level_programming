ffb9bddabb0cffb46920a61c8bb0b720c7d067c1
kidanemariam <kide.fikadu1@gmail.com>
2022-09-21 23:25:36 +0000

    magic calculator

--- 0x05-python-exceptions/102-magic_calculation.py
+++ 0x05-python-exceptions/102-magic_calculation.py
@@ -1,14 +1,4 @@
 #!/usr/bin/python3
-class Error(Exception):
-    """Base class for other exceptions"""
-    pass
-
-
-class ValueTooFar(Error):
-    """Raised when the input value is too large"""
-    pass
-
-
 def magic_calculation(a, b):
     result = 0
     for i in range(1, 3):
@@ -17,7 +7,7 @@ def magic_calculation(a, b):
                 raise Exception("Too far")
             else:
                 result += (a ** b) / i
-        except ValueTooFar:
+        except:
             result = b + a
             break
     return (result)
